840607fa6d9d770f7a299df8518ca58f003bc7a8
Max Kayen <mkayen@gmail.com>
2015-01-26 22:39:09 -0500

    Bug Fix for unidentified pgsgquad

--- query.js
+++ query.js
@@ -38,9 +38,9 @@ for (var i = 0; i < length; i++){
 	projectionArray.push(projection);
 }
 
-// PLAYER ARRAY ------------------------------------------
-// POOLS ALL PLAYER DATA FROM NUMBERFIRE
-// INCLUDING: PLAYER ID, NAME, POSITION
+// // PLAYER ARRAY ------------------------------------------
+// // POOLS ALL PLAYER DATA FROM NUMBERFIRE
+// // INCLUDING: PLAYER ID, NAME, POSITION
 
 for (var i = 0; i < playerLength; i++){
 	if(data["players"][i]!==undefined){
@@ -53,9 +53,9 @@ for (var i = 0; i < playerLength; i++){
 	}
 }
 
-// OMNIBUS ARRAY [COMBINES ALL DATA] ---------------------
-// CONNECTS PROJECTION AND PLAYER DATA VIA PLAYER ID
-// INCLUDING: NAME, POSITION, SALARY, POINTS, RATIO
+// // OMNIBUS ARRAY [COMBINES ALL DATA] ---------------------
+// // CONNECTS PROJECTION AND PLAYER DATA VIA PLAYER ID
+// // INCLUDING: NAME, POSITION, SALARY, POINTS, RATIO
 
 plaLength = playerArray.length
 proLength = projectionArray.length
@@ -70,14 +70,16 @@ for (var l = 0; l < proLength; l++){
 			var ratio = projectionArray[k][3]
 			completePlayer = []
 			completePlayer.push(name, position, salary, points, ratio)
-			omnibus.push(completePlayer)
+			if(points > 10){
+				omnibus.push(completePlayer)
+			}
 		}
 	}
 }
 
-// POSITION FUNCTION --------------------------------------
-// BREAKS DOWN OMNIBUS ARRAY BY POSITION, CREATING POSITIONAL ARRAYS
-// INCLUDING: NAME, POSITION, SALARY, POINTS, RATIO
+// // POSITION FUNCTION --------------------------------------
+// // BREAKS DOWN OMNIBUS ARRAY BY POSITION, CREATING POSITIONAL ARRAYS
+// // INCLUDING: NAME, POSITION, SALARY, POINTS, RATIO
 
 var omnibusLength = omnibus.length
 
@@ -97,9 +99,11 @@ positionFunc('SF', allSF)
 positionFunc('PF', allPF)
 positionFunc('C', allC)
 
-// POSITION PAIRS -----------------------------------------
-// PAIRS UP PLAYERS AT PARTICULAR POSITION TO ACCOUNT FOR
-// TWO STARTERS AT EACH POSITION OTHER THAN CENTER
+//console.log(allPG, allSG, allSF, allPF, allC)
+
+// // POSITION PAIRS -----------------------------------------
+// // PAIRS UP PLAYERS AT PARTICULAR POSITION TO ACCOUNT FOR
+// // TWO STARTERS AT EACH POSITION OTHER THAN CENTER
 
 function positionPairs(origArray, newArray){
 	var length = origArray.length
@@ -124,10 +128,12 @@ positionPairs(allSG, sgAllPairs)
 positionPairs(allSF, sfAllPairs)
 positionPairs(allPF, pfAllPairs)
 
-// POSITION QUADS ------------------------------------------
-// QUADS UP PLAYERS AT TWO POSITIONS.
-// FILTERING IS CONTROLLED BY FINAL IF STATEMENT, WHERE QUAD
-// ARE PUSHED TO ARRAY IF IT MEETS STANDARDS DEFINED.
+
+
+// // POSITION QUADS ------------------------------------------
+// // QUADS UP PLAYERS AT TWO POSITIONS.
+// // FILTERING IS CONTROLLED BY FINAL IF STATEMENT, WHERE QUAD
+// // ARE PUSHED TO ARRAY IF IT MEETS STANDARDS DEFINED.
 
 function positionQuads(pairArray1, pairArray2, newArray){
 	var length1 = pairArray1.length
@@ -143,9 +149,9 @@ function positionQuads(pairArray1, pairArray2, newArray){
 			var points = pairArray1[j][3] + pairArray2[i][3]
 			if(i!=j){
 				posQuad = []
-				posQuad.push(pos1name, pos2name, pos3name, pos4name, price, points)
-				if(price>26000 && price <=28000 && points>125){
-					newArray.push(posQuad)
+				posQuad.push(pos1name, pos2name, pos3name, pos4name, price, points);
+				if(points > 120){
+					newArray.push(posQuad);		
 				}
 			}
 		}
@@ -155,30 +161,30 @@ function positionQuads(pairArray1, pairArray2, newArray){
 positionQuads(pgAllPairs, sgAllPairs, pgsgQuad)
 positionQuads(sfAllPairs, pfAllPairs, sfpfQuad)
 
-// POSITION EIGHTS -----------------------------------------
-// GENERATES TEAMS OF EIGHT. INCLUDING ALL PLAYERS OTHER THAN
-// CENTER.
+// // POSITION EIGHTS -----------------------------------------
+// // GENERATES TEAMS OF EIGHT. INCLUDING ALL PLAYERS OTHER THAN
+// // CENTER.
 
-function positionEights(quadArray1, quadArray2, newArray){
-	var length1 = quadArray1.length;
+function positionEights(pgsgQuad, quadArray2, newArray){
+	var length1 = pgsgQuad.length;
 	var length2 = quadArray2.length;
 	for (var j = 0; j < length1; j++){
-		var i = j + 1
-		for (var i; i < length2; i++){
-			var pos1name = quadArray1[i][0]
-			var pos2name = quadArray1[i][1]
-			var pos3name = quadArray1[i][2]
-			var pos4name = quadArray1[i][3]
-			var pos5name = quadArray2[j][0]
-			var pos6name = quadArray2[j][1]
-			var pos7name = quadArray2[j][2]
-			var pos8name = quadArray2[j][3]
-			var price = quadArray1[i][4] + quadArray2[j][4]
-			var points = quadArray1[i][5] + quadArray2[j][5]
+		var i = j + 1;
+		for (var i = 0; i < length2; i++){
+			var pos1name = pgsgQuad[j][0]
+			var pos2name = pgsgQuad[j][1]
+			var pos3name = pgsgQuad[j][2]
+			var pos4name = pgsgQuad[j][3]
+			var pos5name = quadArray2[i][0]
+			var pos6name = quadArray2[i][1]
+			var pos7name = quadArray2[i][2]
+			var pos8name = quadArray2[i][3]
+			var price = pgsgQuad[j][4] + quadArray2[i][4]
+			var points = pgsgQuad[j][5] + quadArray2[i][5]
 			if(i!=j){
 				posEight = []
 				posEight.push(pos1name, pos2name, pos3name, pos4name, pos5name, pos6name, pos7name, pos8name, price, points)
-				eight.push(posEight)
+				newArray.push(posEight)
 			}
 		}
 	}
@@ -186,52 +192,50 @@ function positionEights(quadArray1, quadArray2, newArray){
 
 positionEights(pgsgQuad, sfpfQuad, eight)
 
-// STARTING 9 ---------------------------------------------
-// GENERATES ARRAY OF STARTING LINEUPS IN CONSIDERATION
-// LINEUPS ARE PUSHED TO ARRAY IF PRICE IS LESS
-// THAN $60K
+// // STARTING 9 ---------------------------------------------
+// // GENERATES ARRAY OF STARTING LINEUPS IN CONSIDERATION
+// // LINEUPS ARE PUSHED TO ARRAY IF PRICE IS LESS
+// // THAN $60K
 
-function starting9(eight, allC, newArray){
-	var length1 = eight.length
-	var length2 = allC.length
-	for (var j = 0; j < length1; j++){
-		var i = j + 1
-		for (var i; i < length2; i++){
-			var pos1name = eight[i][0]
-			var pos2name = eight[i][1]
-			var pos3name = eight[i][2]
-			var pos4name = eight[i][3]
-			var pos5name = eight[i][4]
-			var pos6name = eight[i][5]
-			var pos7name = eight[i][6]
-			var pos8name = eight[i][7]
-			var pos9name = allC[j][0]
-			var price = eight[i][8] + allC[j][2]
-			var points = eight[i][9] + allC[j][3]
-			if(i!=j){
-				start9 = []
-				start9.push(pos1name, pos2name, pos3name, pos4name, pos5name, pos6name, pos7name, pos8name, pos9name, price, points)
-				if(price<=60000){
-					newArray.push(start9)
-				}
+
+var length1 = eight.length
+var length2 = allC.length
+
+for (var j = 0; j < length1; j++){
+	var i = j + 1
+	for (var i; i < length2; i++){
+		var pos1name = eight[i][0]
+		var pos2name = eight[i][1]
+		var pos3name = eight[i][2]
+		var pos4name = eight[i][3]
+		var pos5name = eight[i][4]
+		var pos6name = eight[i][5]
+		var pos7name = eight[i][6]
+		var pos8name = eight[i][7]
+		var pos9name = allC[j][0]
+		var price = eight[i][8] + allC[j][2]
+		var points = eight[i][9] + allC[j][3]
+		if(i!=j){
+			start9 = []
+			start9.push(pos1name, pos2name, pos3name, pos4name, pos5name, pos6name, pos7name, pos8name, pos9name, price, points)
+			if(price <= 60000){
+				lineups.push(start9)
 			}
-		}	
-	}
+		}
+	}	
 }
 
-starting9(eight, allC, lineups)
-
-// SORT ---------------------------------------------------
-// SORTS ALL QUALIFYING STARTING LINEUPS IN ORDER OF HIGHEST
-// TO LOWEST PROJECTED POINTS.
+// // SORT ---------------------------------------------------
+// // SORTS ALL QUALIFYING STARTING LINEUPS IN ORDER OF HIGHEST
+// // TO LOWEST PROJECTED POINTS.
 
 var sortedOut = lineups
 sortedOut.sort(function(a,b){
 	return b[10] - a[10]
 })
 
+console.log(sortedOut.length)
 console.log(sortedOut)
-
 
 
 
